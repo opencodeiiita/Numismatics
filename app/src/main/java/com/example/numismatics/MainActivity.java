a6cffb4e59270976d1eeaa3c61d2a588840f0118
This PC <bansal.yash.130@gmail.com>
2021-10-04 21:19:41 +0530

    Navigation Drawer added

--- app/src/main/java/com/example/numismatics/MainActivity.java
+++ app/src/main/java/com/example/numismatics/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.numismatics;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.internal.NavigationMenuItemView;
+
 
 public class MainActivity extends AppCompatActivity{
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+    public NavigationMenuItemView analytics;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
     }
+    /* override the onOptionsItemSelected() function to implement
+    the item click listener callback to open and close the navigation
+    drawer when the icon is clicked */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            analytics= (NavigationMenuItemView) findViewById(R.id.nav_analytics);
+            analytics.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, AnalyticsActivity.class);
+                    startActivity(intent);
+                }
+            });
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
