5c212ea5a4d13cca806130a27410b0741e023444
Rohan Babbar <77425744+rohanbabbar04@users.noreply.github.com>
2021-10-04 11:19:29 +0530

    Added Transaction Entity/Table (#16)
    * Added Transaction Entity/Table
    
    * Made changes
    
    * Made changes
    
    Co-authored-by: rohanbabbar <rohanbabbar0408@gmail.com>

---
+++ app/src/main/java/com/example/numismatics/TransactionEntity.java
@@ -0,0 +1,70 @@
+package com.example.numismatics;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.time.LocalDate;
+
+@Entity(tableName = "transaction")
+public class TransactionEntity {
+
+    @NonNull
+    @PrimaryKey(autoGenerate = true)
+    private Integer transactionID;
+
+    @ColumnInfo(name = "cost")
+    private Double cost;
+
+    @ColumnInfo(name = "date")
+    private LocalDate date;
+
+    @ColumnInfo(name = "remark")
+    private String remark;
+
+    public TransactionEntity() {
+    }
+
+    public TransactionEntity(@NonNull Integer transactionID, Double cost, LocalDate date, String remark) {
+        this.transactionID = transactionID;
+        this.cost = cost;
+        this.date = date;
+        this.remark = remark;
+    }
+
+    public TransactionEntity(Double cost, LocalDate date, String remark) {
+        this.cost = cost;
+        this.date = date;
+        this.remark = remark;
+    }
+
+    public int getTransactionID() {
+        return transactionID;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+}
