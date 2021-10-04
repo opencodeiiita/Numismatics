30e6b851683a60b10b24fa5f7dd6b7b8f4fc7fef
Tanushree-coder <tanushree950444@gmail.com>
2021-10-04 14:02:17 +0530

    Added two activities

---
+++ app/src/main/java/com/example/numismatics/AddTransaction.java
@@ -0,0 +1,14 @@
+package com.example.numismatics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTransaction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_transaction);
+    }
+}
