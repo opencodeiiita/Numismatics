f8414b8e44747067b3d1d83ffe7c6326fcff0bdd
Yash bansal <bansal.yash.130@gmail.com>
2021-10-05 11:20:34 +0530

    Added Navigation Drawer (#32)
    * Navigation Drawer added
    
    * no change
    
    * Added ActionBar and Navigation Drawer

---
+++ app/src/main/java/com/example/numismatics/AnalyticsActivity.java
@@ -0,0 +1,15 @@
+package com.example.numismatics;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class AnalyticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analytics);
+    }
+}
--- app/src/main/java/com/example/numismatics/MainActivity.java
+++ app/src/main/java/com/example/numismatics/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.numismatics;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.internal.NavigationMenuItemView;
+import com.google.android.material.navigation.NavigationView;
+
 
 public class MainActivity extends AppCompatActivity{
+    public DrawerLayout drawerLayout;
+    public NavigationMenuItemView analytics;
+
+    private RelativeLayout layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout = (RelativeLayout) findViewById(R.id.layout);
+        configureNavigationDrawer();
+        configureToolbar();
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //getMenuInflater().inflate(R.menu.empty_menu, menu);
+        return true;
+    }
+    private void configureToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_nav_menu);
+        actionbar.setDisplayHomeAsUpEnabled(true);
+    }
+    private void configureNavigationDrawer() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.my_drawer_layout);
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                if (itemId == R.id.nav_analytics) {
+                    Intent intent = new Intent(MainActivity.this, AnalyticsActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemId = item.getItemId();
+        switch(itemId) {
+            // Android home
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                    return true;
+            // manage other entries if you have it ...
+        }
+        return true;
     }
 }
