a7e3ecae641dc72add502bbe344a5747e80aec93
This PC <bansal.yash.130@gmail.com>
2021-10-05 10:32:04 +0530

    Added ActionBar and Navigation Drawer

--- app/src/main/java/com/example/numismatics/AnalyticsActivity.java
+++ app/src/main/java/com/example/numismatics/AnalyticsActivity.java
@@ -1,6 +1,7 @@
 package com.example.numismatics;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
--- app/src/main/java/com/example/numismatics/MainActivity.java
+++ app/src/main/java/com/example/numismatics/MainActivity.java
@@ -1,63 +1,83 @@
 package com.example.numismatics;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.google.android.material.internal.NavigationMenuItemView;
+import com.google.android.material.navigation.NavigationView;
 
 
 public class MainActivity extends AppCompatActivity{
     public DrawerLayout drawerLayout;
-    public ActionBarDrawerToggle actionBarDrawerToggle;
     public NavigationMenuItemView analytics;
+
+    private RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // drawer layout instance to toggle the menu icon to open
-        // drawer and back button to close drawer
-        drawerLayout = findViewById(R.id.my_drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
-
-        // pass the Open and Close toggle for the drawer layout listener
-        // to toggle the button
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-
-        // to make the Navigation drawer icon always appear on the action bar
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
+        layout = (RelativeLayout) findViewById(R.id.layout);
+        configureNavigationDrawer();
+        configureToolbar();
 
     }
-    /* override the onOptionsItemSelected() function to implement
-    the item click listener callback to open and close the navigation
-    drawer when the icon is clicked */
+
+
+
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-            analytics= (NavigationMenuItemView) findViewById(R.id.nav_analytics);
-            analytics.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //getMenuInflater().inflate(R.menu.empty_menu, menu);
+        return true;
+    }
+    private void configureToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_nav_menu);
+        actionbar.setDisplayHomeAsUpEnabled(true);
+    }
+    private void configureNavigationDrawer() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.my_drawer_layout);
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                if (itemId == R.id.nav_analytics) {
                     Intent intent = new Intent(MainActivity.this, AnalyticsActivity.class);
                     startActivity(intent);
+                    return true;
                 }
-            });
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+                return false;
+            }
+        });
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-
+        int itemId = item.getItemId();
+        switch(itemId) {
+            // Android home
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                    return true;
+            // manage other entries if you have it ...
+        }
+        return true;
+    }
 }
