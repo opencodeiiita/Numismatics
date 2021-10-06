05bb5deb2456f21654f2ecb7d686d6020ddb9ab9
Tanushree <75807670+Tanushree-coder@users.noreply.github.com>
2021-10-06 12:56:18 +0530

    Update AddTransaction.java

--- app/src/main/java/com/example/numismatics/AddTransaction.java
+++ app/src/main/java/com/example/numismatics/AddTransaction.java
@@ -2,15 +2,7 @@ package com.example.numismatics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.DatePicker;
-import android.widget.EditText;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
 
 public class AddTransaction extends AppCompatActivity {
 
@@ -18,40 +10,6 @@ public class AddTransaction extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_transaction);
-        final Calendar myCalendar = Calendar.getInstance();
-
-        EditText edittext = (EditText) findViewById(R.id.date);
-        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-
-            @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear,
-                                  int dayOfMonth) {
-                // TODO Auto-generated method stub
-                myCalendar.set(Calendar.YEAR, year);
-                myCalendar.set(Calendar.MONTH, monthOfYear);
-                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateLabel();
-            }
-
-            private void updateLabel() {
-                String myFormat = "MM/dd/yy"; //In which you need put here
-                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-
-                edittext.setText(sdf.format(myCalendar.getTime()));
-            }
-
-        };
-
-        edittext.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                new DatePickerDialog(AddTransaction.this, date, myCalendar
-                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-            }
-        });
-
+        
     }
 }
