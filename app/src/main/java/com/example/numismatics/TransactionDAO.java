455e16f246469b21c814246d315aee964f840cba
rohanbabbar <rohanbabbar0408@gmail.com>
2021-10-09 22:26:54 +0530

    Added the getTransactions() method

--- app/src/main/java/com/example/numismatics/TransactionDAO.java
+++ app/src/main/java/com/example/numismatics/TransactionDAO.java
@@ -1,12 +1,19 @@
 package com.example.numismatics;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface TransactionDAO {
 
     @Insert
     void insert(TransactionEntity transaction);
+
+    @Query("SELECT * FROM `transaction` ORDER BY transactionID ASC")
+    LiveData<List<TransactionEntity>> getTransactions();
 
 }
