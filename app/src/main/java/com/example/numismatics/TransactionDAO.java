f32b22c92bd5c115d30449e8390b6334bb3d14f5
Aditi Yadav <adityyyyadav@gmail.com>
2021-10-10 13:49:38 +0530

    Added Query to get a Transaction Item (#63)
    Co-authored-by: adtoria <adityyyyadav@gmal.com>

--- app/src/main/java/com/example/numismatics/TransactionDAO.java
+++ app/src/main/java/com/example/numismatics/TransactionDAO.java
@@ -19,5 +19,7 @@ public interface TransactionDAO {
 
     @Query("DELETE FROM `transaction` WHERE transactionID = :id")
     void deleteById(int id);
-    
+
+    @Query("SELECT * FROM `transaction` WHERE transactionID = :id")
+    TransactionEntity transactionDetails(int id);
 }
