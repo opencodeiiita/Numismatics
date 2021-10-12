8db4eaf0c3d5b8c62796eff14ef098aa1a636e25
Rohan Babbar <77425744+rohanbabbar04@users.noreply.github.com>
2021-10-12 07:36:11 +0530

    Added Collapsing Drawer Property (#71)
    * Clicking Transactions closes the drawer
    
    * Clicking Transactions closes the drawer
    
    Co-authored-by: rohanbabbar <rohanbabbar0408@gmail.com>

--- app/src/main/java/com/example/numismatics/MainActivity.java
+++ app/src/main/java/com/example/numismatics/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.numismatics;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -10,10 +8,8 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.google.android.material.internal.NavigationMenuItemView;
@@ -31,7 +27,7 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        layout = (RelativeLayout) findViewById(R.id.layout);
+        layout = findViewById(R.id.layout);
         configureNavigationDrawer();
         configureToolbar();
 
@@ -45,20 +41,23 @@ public class MainActivity extends AppCompatActivity{
         return true;
     }
     private void configureToolbar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionbar = getSupportActionBar();
         actionbar.setHomeAsUpIndicator(R.drawable.ic_nav_menu);
         actionbar.setDisplayHomeAsUpEnabled(true);
     }
     private void configureNavigationDrawer() {
-        drawerLayout = (DrawerLayout) findViewById(R.id.my_drawer_layout);
-        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        NavigationView navView = findViewById(R.id.nav_view);
         navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
                 int itemId = menuItem.getItemId();
-                if (itemId == R.id.nav_analytics) {
+                if (itemId == R.id.nav_transaction) {
+                    drawerLayout.closeDrawers();
+                    return true;
+                } else if (itemId == R.id.nav_analytics) {
                     Intent intent = new Intent(MainActivity.this, AnalyticsActivity.class);
                     startActivity(intent);
                     return true;
