bf4047190765f5df09c706230d15bfe7b221c32b
raghavgoel25 <>
2021-10-26 12:09:35 +0530

    Issue 154

--- app/src/main/java/com/example/numismatics/TransactionnDetail.java
+++ app/src/main/java/com/example/numismatics/TransactionnDetail.java
@@ -1,6 +1,8 @@
 package com.example.numismatics;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +12,10 @@ public class TransactionnDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transactionn_detail);
+        Toolbar myChildToolbar =
+                findViewById(R.id.transaction_detail);
+        setSupportActionBar(myChildToolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
