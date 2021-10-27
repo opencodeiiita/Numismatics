af0b7a966bb970b677535eec22ad7f75d5a84f51
Aditya Narayan <narayanaditay1007@gmail.com>
2021-10-27 07:32:21 +0530

    bugs removed

--- app/src/main/java/com/example/numismatics/AddTransaction.java
+++ app/src/main/java/com/example/numismatics/AddTransaction.java
@@ -7,12 +7,15 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -20,8 +23,12 @@ import java.util.Locale;
 import java.util.function.DoubleUnaryOperator;
 
 public class AddTransaction extends AppCompatActivity {
+    public static final String EXTRA_AMOUNT="com.example.numismatics.EXTRA_AMOUNT";
+    public static final String EXTRA_REMARK="com.example.numismatics.EXTRA_REMARK";
+    public static final String EXTRA_DATE="com.example.numismatics.EXTRA_DATE";
     private EditText edittext,remark,amount;
     Button Submit;
+    ToggleButton toggle;
     private Calendar myCalendar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,7 @@ public class AddTransaction extends AppCompatActivity {
         amount=findViewById(R.id.add_amount);
         remark=findViewById(R.id.add_remark);
         Submit=findViewById(R.id.add_submit);
+        toggle=findViewById(R.id.add_toggle);
         DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
 
             @Override
@@ -62,22 +70,46 @@ public class AddTransaction extends AppCompatActivity {
             }
 
         });
-        String Remark=remark.getText().toString();
-        String Date=edittext.getText().toString();
-        String Amountstr=amount.getText().toString();
-        double Amount=0.0;
-        if(!Amountstr.isEmpty()){
-            Amount=Double.parseDouble(Amountstr);
-        }
         Submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                saveTransaction();
                 finish();
             }
         });
 
     }
+
+    private void saveTransaction(){
+        String amount1= amount.getText().toString();
+        String remark1=remark.getText().toString();
+        String date1=edittext.getText().toString();
+
+        if(amount1.trim().isEmpty())
+        {
+            Toast.makeText(this, "Please insert amount", Toast.LENGTH_SHORT).show();
+        }
+        if(date1.trim().isEmpty())
+        {
+            Toast.makeText(this, "Please insert date", Toast.LENGTH_SHORT).show();
+        }
+        if(amount1.trim().isEmpty() && date1.trim().isEmpty())
+        {
+            Toast.makeText(this, "Please insert amount and date", Toast.LENGTH_SHORT).show();
+        }
+        if(!toggle.isChecked()){
+            amount1="-"+amount1;
+        }
+
+        Intent data=new Intent();
+        data.putExtra(EXTRA_AMOUNT,amount1);
+        data.putExtra(EXTRA_DATE,date1);
+        data.putExtra(EXTRA_REMARK,remark1);
+        setResult(RESULT_OK,data);
+        finish();
+
+    }
+    
 
     private void updateLabel() {
         String myFormat = "dd/MM/yyyy"; //In which you need put here
